3f85fbcfa95c7a15f45751216e20cdee75f6a10b
Dan Jacoby <jacobydan01@gmail.com>
2024-03-28 20:21:07 -0400

    fcfunctions

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(stayClassy){
+    stayClassy()
+};
+
+// const returnsANamedFunction = () => {
+//     return function namedFunction() {
+      
+//     }
+//   }
+
+function returnsANamedFunction() {
+    return function namedFunction(){
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function(){
+        
+    }
+}
